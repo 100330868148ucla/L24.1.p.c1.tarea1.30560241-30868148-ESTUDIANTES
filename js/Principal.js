2300155f9f88c9e1d84169f4bb1556759398a2a9
100330868148ucla <1003.30868148.ucla@gmail.com>
2024-07-03 10:33:03 -0400

    actualizacion 1.00

---
+++ js/Principal.js
@@ -0,0 +1,17 @@
+/**
+ * En la UCLA, el DCYT desea conocer algunas estadísticas de sus estudiantes. De cada
+ * estudiante se conoce: su cédula, sexo y sus tres notas parciales. Se requiere conocer por el
+ * DCYT: a) nota total obtenida por el estudiante b) la mayor nota obtenida c) De los
+ * estudiantes femeninos cantidad de estudiantes aprobadas, d) promedio de notas
+ * El DCYT suministra la siguiente información demostrativa, para 4 estudiantes: (cedula, sexo,
+ * notap1, notap2, notap3) (13457856, M, 15, 20, 25) (15432551, F, 10, 10, 10) (32455678, M,
+ * 18, 22, 20) (28678900, F, 18, 25, 24), según lo cual la salida requerida presenta el siguiente
+ * formato: 
+ * El estudiante con cedula 13457856 obtuvo una nota total de 60
+ * El estudiante con cedula 15432551 obtuvo una nota total de 30
+ * El estudiante con cedula 32455678 obtuvo una nota total de 60
+ * El estudiante con cedula 28678900 obtuvo una nota total de 67
+ * La mayor nota es 60
+ * Estudiantes femeninas aprobadas 1
+ * Promedio de notas 54.25
+ * */
