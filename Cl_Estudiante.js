f6926ac9e04eae8ab2840a944b2548227a0c7373
100330868148ucla <1003.30868148.ucla@gmail.com>
2024-07-04 00:02:36 -0400

    actualizacion 1.04

---
+++ Cl_Estudiante.js
@@ -0,0 +1,9 @@
+export default class Cl_Estudiante {
+    constructor(CI, Sexo, Np1, Np2, Np3) {
+        this.CI = CI;
+        this.Sexo = Sexo;
+        this.Np1 = Np1;
+        this.Np2 = Np2;
+        this.Np3 = Np3;
+    }
+}
